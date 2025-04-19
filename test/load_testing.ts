6c846221e5bc602c7b5b89c0a527624dcc16691b
Tude Maha <gede_mahardika88@yahoo.co.id>
2025-04-19 16:35:45 +0800

    feat: add smoke and load testing

---
+++ test/load_testing.ts
@@ -0,0 +1,28 @@
+import http from "k6/http";
+import { sleep, check } from "k6";
+
+const file = open(`${__ENV.FILE_UPLOAD}`, "b");
+
+export const options = {
+  stages: [
+    { duration: "5m", target: 25 },
+    { duration: "30m", target: 25 },
+    { duration: "5m", target: 0 },
+  ],
+};
+
+export default function () {
+  const data: http.RequestBody = {
+    field: "file",
+    file: http.file(file, `${__ENV.FILE_NAME}`),
+  };
+
+  const res = http.post(`${__ENV.SERVER_ENDPOINT}/upload`, data);
+  check(res, {
+    "Res status is 200": (r) => res.status === 200,
+    "Res Content-Type header": (r) =>
+      res.headers["Content-Type"] === "application/json",
+  });
+
+  sleep(3);
+}
---
+++ test/smoke_testing.ts
@@ -0,0 +1,25 @@
+import http from "k6/http";
+import { sleep, check } from "k6";
+
+const file = open(`${__ENV.FILE_UPLOAD}`, "b");
+
+export const options = {
+  vus: 1,
+  duration: "30s",
+};
+
+export default function () {
+  const data: http.RequestBody = {
+    field: "file",
+    file: http.file(file, `${__ENV.FILE_NAME}`),
+  };
+
+  const res = http.post(`${__ENV.SERVER_ENDPOINT}/upload`, data);
+  check(res, {
+    "Res status is 200": (r) => res.status === 200,
+    "Res Content-Type header": (r) =>
+      res.headers["Content-Type"] === "application/json",
+  });
+
+  sleep(3);
+}
